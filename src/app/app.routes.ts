2702053a32d08c542b160829dec09ba7c6e2d795
victtor2 <vigarzon@itsqmet.edu.ec>
2025-03-31 00:54:03 -0500

    servicioT

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { CoursesComponent } from './pages/courses/courses.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { VideosComponent } from './pages/videos/videos.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
 
@@ -11,6 +12,7 @@ export const routes: Routes = [
     { path: 'courses', component: CoursesComponent},
     { path: 'courses/:id', component: VideosComponent},
     { path: 'login', component: LoginComponent},
+    { path: 'contact', component: ContactComponent},
     { path: '', redirectTo: '/home', pathMatch: 'full'},
     { path: '**', component: NotFoundComponent}
 
